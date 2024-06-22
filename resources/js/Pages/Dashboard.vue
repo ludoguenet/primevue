2d73924f0822a0740882386e7b63f1ef59a18a59
ludoguenet <ludovicguenet@gmx.com>
2024-06-22 16:30:28 +0200

    fix: added primevue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,17 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import MultiSelect from 'primevue/multiselect';
+import { ref } from "vue";
+
+const selectedCities = ref();
+const cities = ref([
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+]);
 </script>
 
 <template>
@@ -14,7 +25,23 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">
+                        <div class="card flex justify-center">
+                            <MultiSelect
+                                v-model="selectedCities"
+                                :options="cities"
+                                optionLabel="name"
+                                placeholder="Select Cities"
+                                :maxSelectedLabels="3"
+                                class="w-full md:w-[20rem]"
+                                :pt="{
+                                    header: {
+                                        class: ['flex items-center justify-between', 'py-10 px-5', 'm-0', 'border-b', 'rounded-tl-md', 'rounded-tr-md', 'text-surface-700 dark:text-white/80', 'bg-orange-100 dark:bg-surface-800', 'border-surface-300 dark:border-surface-700']
+                                    },
+                                }"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
